b27a1117b280dd5c7b9b94f6840d0f0f19146621
avita <vinaysharma121001@gmail.com>
2022-09-27 16:58:40 +0530

    algorithm

---
+++ alogrithmAndProgramming.js
@@ -0,0 +1,9 @@
+/*Everything is a number or computer only work with numbers
+computer hardware works with 0's and 1's
+we need not to worry about that because of Abstraction:
+Abstraction is a separtion between between What it does and how it does it
+    Interface:What it does
+---------------------------- < Abstraction
+ Implemenation: How it Does it
+ 
+ */
